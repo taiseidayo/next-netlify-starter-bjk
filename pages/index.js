87aa18ac33756524920b203c931571c353b3c94a
taiseidayo <tkapple0827aaa@gmail.com>
2021-12-18 20:30:51 +0900

    re-update.

--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,13 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>This is Deploy test.</title>
+        <title>Welcome, Kaisei!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Welcome to my app!" />
-        <p className="description">Welcome, Kaisei! Let's get started!</p>
+        <p className="description">Let's get started!</p>
       </main>
     </div>
   );
